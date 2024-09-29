e3bd88809c9e4ee578ec661f08039f33c722a38d
nurulislamrimon <nurulislamrimon@gmail.com>
2024-09-29 11:26:20 +0600

    observer added as dependancy holder

--- script.js
+++ script.js
@@ -6,24 +6,33 @@ background-color:red;
 </style>
 <div>
 <label type="checkbox">
-    <input type="checkbox" value="What"/>
+    <input type="checkbox"/>
     <slot></slot>
 </label>
 </div>
 `;
 
 class MyCustomClass extends HTMLElement {
+  checked = "";
   constructor() {
     super();
     this.shadow = null;
   }
 
+  // rendered
   connectedCallback() {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = template;
   }
 
-  attributeChangedCallback() {}
+  // dependancy
+  static get observedAttributes() {
+    return ["name", "id"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log("dependancy updated: ", name, oldValue, newValue);
+  }
 }
 
 customElements.define("ni-rimon", MyCustomClass);
